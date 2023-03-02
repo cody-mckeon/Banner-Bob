12b7cb23f20c13ad934770aad37de984c3a0bc1a
cody-mckeon <codymckeon@gmail.com>
2023-03-02 15:19:32 -0800

    Create React App

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Monetizing the Internet</a>
+      </header>
+      <main>list ads</main>
+    </div>
+  );
+}
+
+export default App;
